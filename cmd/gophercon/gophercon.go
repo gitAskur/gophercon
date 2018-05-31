0241f745049391509bcee0047276bac3d768dcd3
gitAskur <unnar@ccpgames.com>
2018-05-31 11:17:38 +0000

    Added environment variable for service port

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -5,14 +5,20 @@ import (
     "log"
 
     "github.com/gitAskur/gophercon/pkg/routing"
+    "os"
 )
 // go run ./cmd/gophercon/gophercon.go
 func main() {
     log.Printf("Service is starting...")
 
+    port := os.Getenv("SERVICE_PORT")
+    if len(port) == 0 {
+        log.Fatal("Service Port not set!")
+    }
+
     r := routing.BaseRouter()
 
-    log.Fatal(http.ListenAndServe(":8000", r))
+    log.Fatal(http.ListenAndServe(":"+port, r))
 
 }
 
